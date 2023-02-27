62d7c84be822b74c194fc484227c8dfc316af92a
grodetskiy <alex.s.grodetskiy@gmail.com>
2023-02-27 18:36:32 +0200

    Added requirements.txt and setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="UA-Animal-Shelters",
+    version="1.0",
+    author="Alex Grodetskiy",
+    author_email="alex.s.grodetskiy@gmail.com",
+    description="Flask project for EPAM",
+    url="https://github.com/grodetskiy/UA-Animal-Shelters",
+    packages=find_packages(),
+    install_requires=">=3.11"
+)
